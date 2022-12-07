a3eac312f42083f9b983a1b4fecffc83e401746c
kishansripada <kishansripada@gmail.com>
2022-12-07 11:36:19 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({
    Component,
@@ -11,11 +13,18 @@ function MyApp({
    initialSession: Session;
 }>) {
    const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+   let router = useRouter();
+   useEffect(() => {
+      if (!router) return;
 
+      window.location.href = `https://www.formistudio.app${router.asPath}`;
+   }, [router]);
    return (
-      <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-         <Component {...pageProps} />
-      </SessionContextProvider>
+     
+         <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+            <Component {...pageProps} />
+         </SessionContextProvider>
+    
    );
 }
 
