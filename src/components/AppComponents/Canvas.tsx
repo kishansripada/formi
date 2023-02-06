a683becfec7d1976fd6304f785bd835006234b4a
kishansripada <kishansripada@gmail.com>
2023-02-06 16:00:31 -0500

    fix canvas again

--- src/components/AppComponents/Canvas.tsx
+++ src/components/AppComponents/Canvas.tsx
@@ -620,15 +620,16 @@ export const Canvas: React.FC<{
                onPointerDown={!viewOnly ? pointerDown : () => null}
                onPointerMove={handleDragMove}
                ref={stage}
-               className="relative bg-white rounded-3xl "
+               className="relative bg-white rounded-3xl box-content"
                // border-pink-600 border-4 box-border
                style={{
-                  // border: "double 4px transparent",
-                  // // borderImage: "linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)",
-                  // backgroundImage: "linear-gradient(white, white), radial-gradient(circle at top left, #8e24aa,#db2777)",
+                  // boxShadow: "inset 0px 0px 0px 4px #db2777",
+                  border: "solid 4px transparent",
+                  borderImage: "linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)",
+                  backgroundImage: "linear-gradient(white, white), radial-gradient(circle at top left, #8e24aa,#db2777)",
 
                   // backgroundOrigin: "border-box",
-                  // backgroundClip: "padding-box, border-box",
+                  backgroundClip: "padding-box, border-box",
                   // top: scrollOffset.y,
                   // left: scrollOffset.x,
                   // transformOrigin: `${scrollOffset.x}px ${scrollOffset.y}px`,
