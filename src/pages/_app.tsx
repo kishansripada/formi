00f51f33d9cd4180919c57395273937f2123b695
kishansripada <kishansripada@gmail.com>
2023-02-09 14:40:10 -0500

    keys in file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import { useState } from "react";
 import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 
 function MyApp({
@@ -11,7 +10,20 @@ function MyApp({
 }: AppProps<{
    initialSession: Session;
 }>) {
-   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+   const [supabaseClient] = useState(() =>
+      createBrowserSupabaseClient({
+         supabaseKey:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dHhieGtrdm9zbGNyc3hiZmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE0NjM3NDYsImV4cCI6MTk3NzAzOTc0Nn0.caFbFV4Ck7MrTSwsPXyIifjeKWYJWXisKR9-zFA33Ng",
+         supabaseUrl: "https://dxtxbxkkvoslcrsxbfai.supabase.co",
+         options: {
+            realtime: {
+               params: {
+                  eventsPerSecond: 5,
+               },
+            },
+         },
+      })
+   );
 
    return (
       <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
