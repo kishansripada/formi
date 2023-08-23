290a68e5abf59aa83e77290bbc3891190089860e
kishansripada <kishansripada@gmail.com>
2023-08-23 10:40:52 -0400

    fix set formations missing

--- src/app/[danceId]/edit/_components/Formation.tsx
+++ src/app/[danceId]/edit/_components/Formation.tsx
@@ -32,8 +32,7 @@ export const Formation: React.FC<{
    localSettings,
    selectedFormation,
 }) => {
-   const { viewOnly } = useStore();
-
+   const { viewOnly, setFormations } = useStore();
    const others = useStore((state) => state.liveblocks.others);
    const othersOnThisFormation = others.filter((other) => other.presence.selectedFormation === index);
    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
