e66f03a04079e290f532ae965b37c49ac465781e
kishansripada <kishansripada@gmail.com>
2023-09-07 12:02:19 -0400

    better middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,13 @@ import type { NextRequest } from "next/server";
 // import type { Database } from '@/lib/database.types'
 
 export async function middleware(req: NextRequest) {
+   const res = NextResponse.next();
    try {
-      const res = NextResponse.next();
       const supabase = createMiddlewareClient({ req, res });
       await supabase.auth.getSession();
       return res;
    } catch {
+      res.cookies.delete("sb-dxtxbxkkvoslcrsxbfai-auth-token");
       return NextResponse.redirect("/auth/logout");
    }
 }
