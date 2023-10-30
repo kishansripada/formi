b593a8366b06c57b3b7abfd65889c6eb6ae555fa
kishansripada <kishansripada@gmail.com>
2023-10-30 15:29:38 -0400

    loading screen between routes

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+   return (
+      <>
+         <div className="flex items-center justify-center h-screen bg-neutral-900 ">
+            <img src="/logo.png" className="w-12" alt="" />
+         </div>
+      </>
+   );
+}
