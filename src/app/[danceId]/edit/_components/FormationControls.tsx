6a5decf7d97df6c53b4a3feefea16ccb1b83f57c
kishansripada <kishansripada@gmail.com>
2023-11-05 19:03:09 -0500

    collapse formation controls

--- src/app/[danceId]/edit/_components/FormationControls.tsx
+++ src/app/[danceId]/edit/_components/FormationControls.tsx
@@ -4,7 +4,14 @@ import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useStore } from "../store";
 import { Slider } from "../../../../../@/components/ui/slider";
-
+import {
+   DropdownMenu,
+   DropdownMenuContent,
+   DropdownMenuItem,
+   DropdownMenuLabel,
+   DropdownMenuSeparator,
+   DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 export const FormationControls: React.FC<{
    zoom: number;
    selectedDancers: string[];
@@ -24,14 +31,19 @@ export const FormationControls: React.FC<{
       splitSelectedFormations,
    } = useStore();
    return (
-      <div className="w-full h-[40px] min-h-[40px] max-h-[40px]  border-t-neutral-300 border-t bg-neutral-50 md:flex hidden flex-row items-center justify-between px-3 dark:bg-neutral-900 backdrop-blur-md mt-auto   dark:border-neutral-700 dark:text-white">
+      <div className="w-full h-[40px] min-h-[40px] max-h-[40px]  border-t-neutral-300 border-t bg-neutral-50 md:flex hidden flex-row items-center justify-between px-3  dark:bg-neutral-900 backdrop-blur-md mt-auto   dark:border-neutral-700 dark:text-neutral-200">
          {!viewOnly ? (
-            <div className="flex flex-row items-center">
+            <div
+               style={{
+                  display: selectedFormations.length ? "flex" : "none",
+               }}
+               className="flex flex-row items-center gap-4"
+            >
                <button
                   onClick={() => {
                      deleteSelectedFormations();
                   }}
-                  className="   text-sm shadow-sm   cursor-pointer select-none rounded-md font-semibold  grid place-items-center  bg-opacity-20 py-1 px-3 mr-4 bg-red-500 dark:text-red-400 text-red-600  "
+                  className="   text-sm shadow-sm   cursor-pointer select-none rounded-md font-semibold  grid place-items-center  bg-opacity-20 py-1 px-3  bg-red-500 dark:text-red-400 text-red-600  "
                >
                   {selectedFormations.length === 1 ? "Delete Formation" : "Delete Formations"}
                </button>
@@ -59,86 +71,87 @@ export const FormationControls: React.FC<{
 
                   <p className="text-sm">Split</p>
                </button>
-               <button
-                  onClick={() => {
-                     if (!selectedFormations.length) return;
 
-                     setFormations(
-                        formations.map((formation) => {
-                           if (selectedFormations.includes(formation.id)) {
-                              return {
-                                 ...formation,
-                                 positions: formation.positions.map((position) => {
-                                    if (selectedDancers.length && !selectedDancers.includes(position.id)) return position;
-                                    return { ...position, position: { x: position.position.x, y: -position.position.y } };
-                                 }),
-                              };
-                           }
-                           return formation;
-                        })
-                     );
-
-                     // pushChange();
-                  }}
-                  className="rounded-md  hidden transition duration-300 ml-3 lg:flex  flex-row items-center  px-2 py-2  cursor-pointer "
-               >
-                  <svg className="w-5 h-5 mr-1 dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
-                     <path d="M80 606v-60h800v60H80Zm210-120V386h380v100H290Zm0 280V666h380v100H290Z" />
-                  </svg>
-                  <p className="text-sm">Flip X</p>
-               </button>
-               <button
-                  onClick={() => {
-                     if (!selectedFormations.length) return;
-
-                     setFormations(
-                        formations.map((formation) => {
-                           if (selectedFormations.includes(formation.id)) {
-                              return {
-                                 ...formation,
-                                 positions: formation.positions.map((position) => {
-                                    if (selectedDancers.length && !selectedDancers.includes(position.id)) return position;
-                                    return { ...position, position: { x: -position.position.x, y: position.position.y } };
-                                 }),
-                              };
-                           }
-                           return formation;
-                        })
-                     );
-
-                     // pushChange();
-                  }}
-                  className="rounded-md  hidden transition duration-300    lg:flex  flex-row items-center  px-2 py-2  cursor-pointer "
-               >
-                  <svg className="w-5 h-5 mr-1 dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
-                     <path d="M450 976V176h60v800h-60Zm120-210V386h100v380H570Zm-280 0V386h100v380H290Z" />
-                  </svg>
-                  <p className="text-sm">Flip Y</p>
-               </button>
-               {selectedFormations.length === 1 ? (
-                  <button
-                     title="Reset to previous formation"
-                     onClick={() => {
-                        setFormations(
-                           formations.map((formation, index) => {
-                              if (selectedFormations.includes(formation.id)) {
-                                 return {
-                                    ...formation,
-                                    positions: formations[index === 0 ? 0 : index - 1].positions,
-                                 };
-                              }
-                              return formation;
-                           })
-                        );
-                     }}
-                     className="rounded-md  hidden transition duration-300 mr-auto    lg:flex  flex-row items-center  px-2 py-2  cursor-pointer "
-                  >
-                     <svg className="w-5 h-5 mr-1 dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-                        <path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" />
+               <DropdownMenu>
+                  <DropdownMenuTrigger className="text-sm flex flex-row items-center">
+                     More{" "}
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 ml-1">
+                        <path
+                           fillRule="evenodd"
+                           d="M14.77 12.79a.75.75 0 01-1.06-.02L10 8.832 6.29 12.77a.75.75 0 11-1.08-1.04l4.25-4.5a.75.75 0 011.08 0l4.25 4.5a.75.75 0 01-.02 1.06z"
+                           clipRule="evenodd"
+                        />
                      </svg>
-                     <p className="text-sm">Reset</p>
-                  </button>
-               ) : null}
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent side="top">
+                     <DropdownMenuItem
+                        onClick={() => {
+                           setFormations(
+                              formations.map((formation, index) => {
+                                 if (selectedFormations.includes(formation.id)) {
+                                    return {
+                                       ...formation,
+                                       positions: formations[index === 0 ? 0 : index - 1].positions,
+                                    };
+                                 }
+                                 return formation;
+                              })
+                           );
+                        }}
+                     >
+                        <svg className="w-5 h-5 mr-2 dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                           <path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" />
+                        </svg>
+                        Reset to previous formation
+                     </DropdownMenuItem>
+                     <DropdownMenuItem
+                        onClick={() => {
+                           setFormations(
+                              formations.map((formation) => {
+                                 if (selectedFormations.includes(formation.id)) {
+                                    return {
+                                       ...formation,
+                                       positions: formation.positions.map((position) => {
+                                          if (selectedDancers.length && !selectedDancers.includes(position.id)) return position;
+                                          return { ...position, position: { x: position.position.x, y: -position.position.y } };
+                                       }),
+                                    };
+                                 }
+                                 return formation;
+                              })
+                           );
+                        }}
+                     >
+                        <svg className="w-5 h-5 mr-2 dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
+                           <path d="M80 606v-60h800v60H80Zm210-120V386h380v100H290Zm0 280V666h380v100H290Z" />
+                        </svg>
+                        Flip {selectedDancers.length ? "selected" : null} dancers across X axis
+                     </DropdownMenuItem>
+                     <DropdownMenuItem
+                        onClick={() => {
+                           setFormations(
+                              formations.map((formation) => {
+                                 if (selectedFormations.includes(formation.id)) {
+                                    return {
+                                       ...formation,
+                                       positions: formation.positions.map((position) => {
+                                          if (selectedDancers.length && !selectedDancers.includes(position.id)) return position;
+                                          return { ...position, position: { x: -position.position.x, y: position.position.y } };
+                                       }),
+                                    };
+                                 }
+                                 return formation;
+                              })
+                           );
+                        }}
+                     >
+                        <svg className="w-5 h-5 mr-2 dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
+                           <path d="M450 976V176h60v800h-60Zm120-210V386h100v380H570Zm-280 0V386h100v380H290Z" />
+                        </svg>
+                        Flip {selectedDancers.length ? "selected" : null} dancers across Y axis
+                     </DropdownMenuItem>
+                  </DropdownMenuContent>
+               </DropdownMenu>
             </div>
          ) : null}
 
