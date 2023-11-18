9faec1bcf578d271a171c981e68ddad8ef469b4a
kishansripada <kishansripada@gmail.com>
2023-11-18 13:00:36 -0500

    add error page for sentry

---
+++ src/app/error.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import * as Sentry from "@sentry/nextjs";
+import type { NextPage } from "next";
+import type { ErrorProps } from "next/error";
+import { useEffect } from "react";
+import { Button } from "../../@/components/ui/button";
+
+const Error = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
+   useEffect(() => {
+      // Log the error to an error reporting service
+      console.error(error);
+   }, [error]);
+   return (
+      <div className="flex flex-col h-screen items-center justify-center gap-4">
+         <h2>Something went wrong!</h2>
+         <Button
+            onClick={
+               // Attempt to recover by trying to re-render the segment
+               () => reset()
+            }
+         >
+            Try again
+         </Button>
+      </div>
+   );
+};
+
+Error.getInitialProps = async (contextData) => {
+   // In case this is running in a serverless function, await this in order to give Sentry
+   // time to send the error before the lambda exits
+   await Sentry.captureUnderscoreErrorException(contextData);
+
+   // This will contain the status code of the response
+   return Error.getInitialProps(contextData);
+};
+
+export default Error;
