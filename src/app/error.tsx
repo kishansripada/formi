a98e2a81be8594c66f1491c2adb5d89b408c43ff
kishansripada <kishansripada@gmail.com>
2023-11-18 13:29:49 -0500

    better error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -8,21 +8,29 @@ import { Button } from "../../@/components/ui/button";
 
 const Error = ({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) => {
    useEffect(() => {
-      Sentry.captureException(error);
+      Sentry.captureException(error, {
+         tags: {
+            section: "error.tsx",
+         },
+      });
       //   Log the error to an error reporting service
       console.error(error);
    }, [error]);
    return (
       <div className="flex flex-col h-screen items-center justify-center gap-4">
-         <h2>Something went wrong!</h2>
+         <h2>Oops! Something went wrong</h2>
+         <p className="text-sm">This error was reported to us and we are currently investigating </p>
+         <p className="text-sm">Email us at: kishansripada@formistudio.app and we will solve it ASAP</p>
+
          <Button
             onClick={
                // Attempt to recover by trying to re-render the segment
                () => reset()
             }
          >
-            Try again
+            Click here to attempt to fix the error
          </Button>
+         <p className="border-2 border-red-500 rounded-md p-2 mt-auto fixed bottom-10">{error.message}</p>
       </div>
    );
 };
