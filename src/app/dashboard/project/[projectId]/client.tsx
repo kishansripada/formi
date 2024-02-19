bb18896e90b1629d354294f51a11a475967d982d
kishansripada <kishansripada@gmail.com>
2024-02-19 11:21:20 -0500

    scroll in project

--- src/app/dashboard/project/[projectId]/client.tsx
+++ src/app/dashboard/project/[projectId]/client.tsx
@@ -4,7 +4,7 @@ import { PerformancePreview } from "../../_components/PerformancePreview";
 
 export default function PageClient({ myDances, session }) {
    return (
-      <div className="px-4 py-5 gap-5 flex flex-col">
+      <div className="px-4 py-5 gap-5 flex flex-col overflow-y-scroll h-full">
          <p className="text-3xl font-semibold">{myDances?.[0]?.project_id?.name || "No performances here yet"}</p>
          <div className="w-full grid grid-cols-1 gap-[32px]   rounded-xl sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 col-span-4   overscroll-contain items-center">
             {myDances.length ? (
